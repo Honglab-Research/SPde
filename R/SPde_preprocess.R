d6bbfbe31e56ad66d240501038dd20ef08a6cf48
Honglab-Research <79962288+Honglab-Research@users.noreply.github.com>
2023-09-21 13:27:38 +0900

    Create SPde_preprocess.R

---
+++ R/SPde_preprocess.R
@@ -0,0 +1,129 @@
+#' @title PrepareCERES
+#' @description
+#' Pre-process CERES data
+#'
+#' @param eff of CRISPR_gene_effect file
+#'
+#' @return A data.frame of eff
+#'
+#' @export
+PrepareCERES <- function(eff){
+  
+  #annotate row and col name
+  cells.ind <- eff[[1]]
+  rownames(eff) <- cells.ind
+  
+  col.eff <- colnames(eff)[-1]
+  col.eff <- sub("\\.\\.([0-9]+)\\.","_\\1",col.eff)
+  
+  sym.eff <- sub("_.+","",col.eff)
+  eids.eff <- sub(".+_","",col.eff
+                  
+  eff <- eff[,-1]
+  colnames(eff) <- sym.eff 
+  eff <- t(eff)
+
+  return(eff)
+}
+
+#' @title PrepareAnnotationFile
+#' @description
+#' Prepare annotation file 
+#'
+#' @param annot of Annotation.version2.xlsx
+#'
+#' @return A data.frame of annot
+#'
+#' @export
+#
+PrepareAnnotationFile <- function(annot){
+  annot = annot[-1,]
+
+  gdsc_depid = annot[which(annot$CRISPR_gene_effect ==1 & annot$GDSC2_public_raw_data_24Jul22==1& annot$`논문_TCGA _annot(from sheet2)` %in%  toupper(cancers_for_eg )& annot$CCLE_expression ==1),]$'DepMap ID'
+
+  ccle_depid = annot[which(annot$CRISPR_gene_effect ==1 & annot$`CCLE_Drug data(CCLE_NP24.2009_Drug_data_2015.02.24)`==1& annot$CCLE_expression ==1 & annot$`논문_TCGA _annot(from sheet2)` %in% toupper(cancers_for_eg )),]$'DepMap ID'
+
+  select = (unique(c(ccle_depid, gdsc_depid)))
+
+  annot = annot[which(annot$`DepMap ID` %in% select),]
+  colnames(annot)[1] = 'Depmap_ID' 
+  colnames(annot)[22] ='Annotation'
+  
+  return (annot)
+}
+
+#' @title Preprocess
+#' @description
+#' Pre-process expression data
+#'
+#' @param exp of merged expression file(TCGA-GBM : NGS, Agilent)
+#'
+#' @return A data.frame of norm_exp
+#'
+#' @export
+#
+preprocess <- function(exp){
+  gene = rownames(exp)
+  exp = apply(exp,2, as.numeric)
+  
+  rownames(exp) = gene
+  exp = exp[which(rownames(exp)!=""),] 
+
+  #Remove genes for which all samples have a value of 0.
+  cnt.zero= rowSums(exp == 0)
+  zero_exp = exp[which(cnt.zero != ncol(exp)),]
+  
+  #Normaliztion
+  norm_exp = t(scale(t(zero_exp)))
+  
+  return(norm_exp)
+}
+
+#' @title deg
+#' @description
+#' DEG anlaysis using limma 
+#'
+#' @param exp
+#' @param metadata of TCGA-GBM phenotype
+#'
+#' @return list of exp_TN(preprocessed expression data), Fgenes(Final significantly differental expressed genes) , sam(sampleID used in anlysis)
+#'
+#' @export
+#
+deg <- function(exp,metadata){
+  metadata = metadata[which(metadata$sampleID %in% colnames(exp)),c('sampleID','sample_type')]
+  
+  sam = metadata[which(metadata$sample_type != ''),]$sampleID
+  exp_TN = exp[,sam] 
+  
+  rownames(metadata) = metadata$sampleID
+  metadata = (metadata[colnames(exp_TN),]) 
+  
+  only_N = metadata[which(metadata$sample_type == 'Solid Tissue Normal'),]$sampleID
+  group = sapply(colnames(exp_TN), function(x) ifelse(x %in% only_N, 'Normal','Tumor')) %>% as.data.frame()
+  rownames(group) = colnames(exp_TN)  
+  
+  #limma
+  design <- model.matrix(~0+group$.)
+  colnames(design) <-c('Normal','Tumor') #check always 
+  fit <- lmFit(exp_TN, design)
+  cont <- makeContrasts(Tumor-Normal, levels=design)
+  fit.cont <- contrasts.fit(fit, cont)
+  fit.cont <- eBayes(fit.cont)
+  res <- topTable(fit.cont, number = Inf)
+  dim(res) 
+
+  #Extract significantly differential expressed genes between normal and tumor
+  upres = res[which(res$logFC >= 1),]
+  dnres = res[which(res$logFC <= -1),]
+  f_res = rbind(upres,dnres)
+  
+  g= rownames(f_res[which(f_res$P.Value <=0.05),])
+  f_res = f_res[g,]
+  
+  Fgenes = rownames(f_res)
+  exp_TN = as.data.frame(exp_TN)
+  colnames(exp_TN) = sam
+  
+  return(list(exp_TN,Fgenes,sam))
+}
